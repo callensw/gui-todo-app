3e71166e96ea19111bc6091661d89d593909f6ef
Chase Allensworth <chase.allensworth@walmart.com>
2023-09-03 18:06:23 -0500

    1st Commit

---
+++ gui-todo-app/modules/functions.py
@@ -0,0 +1,20 @@
+import os
+
+# Get the directory of the current script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# Construct the absolute path to todos.txt
+FILEPATH = os.path.join(SCRIPT_DIR, "../files/todos.txt")
+
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of
+    to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
